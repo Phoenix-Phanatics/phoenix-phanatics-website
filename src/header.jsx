5b08a8be4eed07c3afecf9882982f1c39e5bb635
abominablepug <esnahoum@gmail.com>
2025-08-11 20:25:28 -0400

    improved header

--- src/header.jsx
+++ src/header.jsx
@@ -1,11 +1,11 @@
-import headerImg from "../images/india.jpg"
-
 export default function Header() {
     return (
         <div className="bg-cover bg-center bg-[url(../images/india.jpg)] h-[80dvh] w-full flex flex-col items-center justify-center gap-8 col-span-3 mask-b-from-80%" id="home">
-            <HeaderText>Team 11104</HeaderText>
-            <hr className="border-4 w-full" />
-            <HeaderText>Phoenix Phanatics</HeaderText>
+            <div className="backdrop-brightness-50 flex flex-col items-center justify-center rounded-3xl">
+                <HeaderText>Team 11104</HeaderText>
+                <hr className="border-4 w-4/5 text-center text-white" />
+                <HeaderText>Phoenix Phanatics</HeaderText>
+            </div>
         </div>
     )
 }
@@ -13,7 +13,7 @@ export default function Header() {
 function HeaderText({ children }) {
     return (
         <>
-            <h1 className="text-7xl text-center m-8 bg-white">{children}</h1>
+            <h1 className="text-9xl text-center m-8 text-white">{children}</h1>
         </>
     )
 }
